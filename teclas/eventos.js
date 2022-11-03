c75bdb8aafa604c650aa59c706cdd425423c9d20
NicoAquino67 <nicoanriquez23@gmail.com>
2022-11-03 21:05:06 +0100

    DONE: movement with the keyboard, 'wasd', arrows and the numpad! Welldone!

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -18,33 +18,56 @@ const MoveKeys = {
         Right:102
     }
 }
-document.addEventListener("keyup", dibujarTeclado);
+document.addEventListener("keydown", dibujarTeclado);
+const canvas = document.getElementById("area-de-dibujo");
+const paper = canvas.getContext("2d");
+var x = 150;
+var y = 150;
+const speed = 2;
+//TODO: crear la funcion y variables necesarias para que el canvas sea una pizarra donde puedes dibujar desde el navegador, o inclusive desde el movil.crac
+
 
 function dibujarTeclado(evento){
+    let color = "rgba(200,20,150,1)";
     switch (evento.keyCode)
     {
         case MoveKeys.flechas.Up:
         case MoveKeys.wasd.Up:
         case MoveKeys.numPad.Up:
-            console.log("Arriba");
+            dibujarLinea(color, x,y,x,y-speed,paper);
+            y = y - speed;
         break
 
         case MoveKeys.flechas.Down:
         case MoveKeys.wasd.Down:
         case MoveKeys.numPad.Down:
-            console.log("Abajo");
+            dibujarLinea(color, x,y,x,y+speed,paper);
+            y = y + speed;
         break
 
         case MoveKeys.flechas.Right:
         case MoveKeys.wasd.Right:
         case MoveKeys.numPad.Right:
-            console.log("Derecha");
+            dibujarLinea(color, x,y,x+speed,y,paper);
+            x = x + speed;
         break
 
         case MoveKeys.flechas.Left:
         case MoveKeys.wasd.Left:
         case MoveKeys.numPad.Left:
-            console.log("Izquierda");
+            dibujarLinea(color, x,y,x-speed,y,paper);
+            x = x - speed;
         break
     }
+}
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 2;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
 }
